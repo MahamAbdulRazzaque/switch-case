9bc7b2b9d2f88772a2552166ca736dfac45812c6
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-11-29 16:57:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+// // Local Variable
+// let local = "I'm Local";
+// function variable() {
+//     document.write(local);
+// }
+// variable();
+
+
+// switch (new Date().getDay()) {
+//     case 0:
+//         document.write("Sunday");
+//         break;
+//     case 1:
+//         document.write("Monday");
+//         break;
+//     case 2:
+//         document.write("Tuesday");
+//         break;
+//     case 3:
+//         document.write("Wednesday");
+//         break;
+//     case 4:
+//         document.write("Thursday");
+//         break;
+//     case 5:
+//         document.write("Friday");
+//         break;
+//     case 6:
+//         document.write("Saturday");
+//         break;
+//     default:
+//         document.write("Invalid day");
+//         break;
+// }
+
+
+// let favfruit = prompt("Which your Favourite fruit..!! \n 1: Watermelon \n 2: Grapes \n 3: Cherry \n 4: Apples \n 5: cheeku");
+// switch (favfruit) {
+//     case '1':
+//        document.write("Watermelon");
+//         break;
+//     case '2':
+//         document.write("Grapes");
+//         break;
+//     case '3':
+//         document.write("Cherry");
+//     break;
+//     case '4':
+//         document.write("Apples");
+//         break;
+//     case '5':
+//         document.write("Cheeku");
+//         break;
+
+//     default:
+//         document.write("Don't Have");
+//         break;
+// }
+
+
+// parseInt
+// let num1 = 15;
+// let num2 = 5;
+// document.write("The sum is       " , num1 + num2 );
+
